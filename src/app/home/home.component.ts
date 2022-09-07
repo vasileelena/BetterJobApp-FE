2b002590f97be52424e950215d327dbd8f77c707
Elena <elle.alexxandra@gmail.com>
2022-09-07 14:34:22 +0300

    landing-page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../services/user.service";
+import {Job} from "../models/job.model";
+import {User} from "../models/user.model";
+import {finalize} from "rxjs";
+import {map} from "rxjs/operators";
+import {NgbModal, NgbModalOptions} from "@ng-bootstrap/ng-bootstrap";
+import {SignInModalComponent} from "./signin-modal/sign-in-modal.component";
+import {LogInModalComponent} from "./login-modal/log-in-modal.component";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +15,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  recentJobs: any[] = [];
+  isInitialised: boolean = false;
+
+  constructor(private userService: UserService,
+              private modalService: NgbModal) {
+  }
 
   ngOnInit(): void {
+    this.initRecentJobs();
+  }
+
+  openSigninModal() {
+    const modalOptions: NgbModalOptions = {backdrop: 'static', size: 'md'}
+    this.modalService.open(SignInModalComponent, modalOptions);
+
+  }
+
+  openLoginModal() {
+    const modalOptions: NgbModalOptions = {backdrop: 'static', size: 'md'}
+    this.modalService.open(LogInModalComponent, modalOptions);
+  }
+
+  private initRecentJobs(): void {
+    this.userService.getAllJobs().pipe(
+      map((jobs: Job[]) => {
+        this.recentJobs =
+          jobs
+            .sort(
+              (a: Job, b: Job) => Number(new Date(b.creationDate)) - Number(new Date(a.creationDate)))
+            .splice(0, 4);
+
+        for (let job of this.recentJobs) {
+          this.userService.getUserById(job.recruiterId)
+            .pipe(map((recruiter: User) => job.company = recruiter.company)).subscribe();
+        }
+      }),
+      finalize(() => this.isInitialised = true)
+    ).subscribe();
   }
 
 }
